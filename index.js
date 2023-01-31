e3253c0984a84d43821a23be5ccbffc54f93841e
Arpita Roy <arpita.roy.workar@gmail.com>
2023-01-31 19:05:49 +0530

    Callback functions intro

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-/**
- Challenge: Add a button that, when clicked, gets a new deck of cards from the deckofcards API
-
- URL: https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/
-
- Log the whole response to the console
- */
-
 document.getElementById("new-deck").addEventListener("click", function() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => console.log(data))
 })
+
+// Functions are a first-class object in JavaScript
+
+let myNum = 42
+let myNum2 = myNum
+
+function myFunction(a, b) {
+    console.log(a + b)
+}
+
+let myFunction2 = myFunction
+myFunction2(1, 2)
