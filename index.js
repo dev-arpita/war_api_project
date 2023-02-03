d890824bb9f5b48290acbe94136e86f8571c0186
Arpita Roy <arpita.roy.workar@gmail.com>
2023-02-03 09:08:29 +0530

    Callbacks - put our custom filterArray function to use

--- index.js
+++ index.js
@@ -6,16 +6,53 @@ function handleClick() {
 
 document.getElementById("new-deck").addEventListener("click", handleClick)
 
-/**
- * Challenge:
- *
- * Part 1: write a `setTimeout` command. Have it wait for 2000 ms before logging "I finally ran!" to the console
- *
- * Part 2: Move the anonymous in-line function to its own, named function
- */
+// function callback() {
+//     console.log("I finally ran!")
+// }
 
-function callback() {
-    console.log("I finally ran!")
+// setTimeout(callback, 2000)
+
+// const people = [
+//     { name: "Jack", hasPet: true },
+//     { name: "Jill", hasPet: false },
+//     { name: "Alice", hasPet: true },
+//     { name: "Bob", hasPet: false },
+// ]
+
+// function gimmeThePets(number) {
+//     return person.hasPet
+// }
+
+// const peopleWithPets = people.filter(gimmeThePets)
+// console.log(peopleWithPets)
+
+const people = [
+    { name: "Jack", hasPet: true },
+    { name: "Jill", hasPet: false },
+    { name: "Alice", hasPet: true },
+    { name: "Bob", hasPet: false },
+]
+
+function filterArray(array, callback) {
+    const resultingArray = []
+    // Write your filtering logic here
+    for (let item of array) {
+        const shouldBeIncluded = callback(item)
+        if (shouldBeIncluded) {
+            resultingArray.push(item)
+        }
+    }
+    return resultingArray
 }
 
-setTimeout(callback, 2000)
+/**
+ * Challenge: Use your filter array method!
+ * Given the above `people` array, return a new array with only people where `hasPet` is true
+ * Note: Remember that your callback function will be given the individual item in the array for a parameter
+ */
+
+const peopleWithPets = filterArray(people, function(person) {
+    return person.hasPet
+})
+
+console.log(peopleWithPets)
